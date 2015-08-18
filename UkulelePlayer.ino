06209c9c82fa472c012ae7b1738d5bb3e7e47d35
Adrian Matejov <a.matejov@centrum.sk>
2015-08-18 13:43:20 +0200

    update: added kruty krtek joy
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- UkulelePlayer.ino
+++ UkulelePlayer.ino
@@ -1,8 +1,10 @@
+/* temporary solution for mlady tvorca */
+
 #include <Servo.h>
 
 #define SERVOS 5
 #define WRIST_DELAY 100
-#define HAND_DELAY 360
+// #define HAND_DELAY 250
 
 #define LED 13
 
@@ -15,9 +17,11 @@ uint8_t pins[] = {2, 3, 4, 5, 6};
 char chord_ids[] = {' ', 'C', 'G', 'a', 'F'};
 uint8_t chords[][SERVOS] = { {90, 90, 90, 90, 100},
                              {90, 90, 90, 40, 100},
-                             {90, 150, 140, 150, 100},
+                             {90, 130, 130, 130, 100},
                              {50, 90, 90, 90, 100},
                              {50, 90, 90, 90, 50} };
+
+uint16_t HAND_DELAY = 360;
 
 /* transform chord (char) into number (uint) */
 uint8_t transform(char chord)
@@ -61,7 +65,8 @@ void led(void)
     digitalWrite(LED, LOW);
 }
 
-void strings(uint8_t count)
+/* song_number - 0 - let it be, 1 - kruty krtek joy */
+void strings(uint8_t count, uint8_t song_number)
 {
     for (uint8_t i = 0; i < count; i++) {
         wrist_on();
@@ -69,10 +74,32 @@ void strings(uint8_t count)
         move_hand('d');
         led();
         delay(HAND_DELAY);
+        
         wrist_off();
         delay(WRIST_DELAY);
         move_hand('u');
         delay(HAND_DELAY);
+        
+        if (song_number == 1) {
+            wrist_on();
+            delay(WRIST_DELAY);
+            move_hand('d');
+            delay(HAND_DELAY);
+
+            move_hand('u');
+            led();
+            delay(HAND_DELAY);
+            
+            wrist_off();
+            delay(WRIST_DELAY);
+            move_hand('d');
+            delay(HAND_DELAY);
+    
+            wrist_on();
+            delay(WRIST_DELAY);
+            move_hand('u');
+            delay(HAND_DELAY);
+        }
     }
 }
 
@@ -92,13 +119,27 @@ void setup()
     delay(WRIST_DELAY);
     move_hand('u');
     delay(500);
+    Serial.begin(115200);
 }
 
-char chords_to_play[] = {'C', 'G', 'a', 'F', 'C', 'G', 'a', 'C'};
+char let_it_be[] = {'C', 'G', 'a', 'F', 'C', 'G', 'a', 'C'};
+char krtek[] = {'a', 'a', 'a', 'G', 'G', 'G', 'a', 'F', 'C', 'G', 'a', 'F', 'C', 'G', 'a'};
 void loop()
 {
-    for (uint8_t i = 0; i < (sizeof(chords_to_play)/sizeof(*chords_to_play)); i++) {
-        play(chords_to_play[i]);
-        strings(2);
+    for (uint8_t j = 0; j < 3; j++) {
+        HAND_DELAY = 360;
+        for (uint8_t i = 0; i < (sizeof(let_it_be)/sizeof(*let_it_be)); i++) {
+            play(let_it_be[i]);
+            strings(2, 0);
+        }
     }
+    
+    for (uint8_t j = 0; j < 3; j++) {
+        HAND_DELAY = 250;
+        for (uint8_t i = 0; i < (sizeof(krtek)/sizeof(*krtek)); i++) {
+            play(krtek[i]);
+            strings(1, 1);
+        }
+    }
+    
 }
